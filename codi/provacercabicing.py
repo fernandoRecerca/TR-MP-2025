d7c3f9b83918bbffde891478a57b9261a1f3dab9
Max <maxperezsastre@gmail.com>
2025-08-27 03:59:17 +0200

    per fi torna a funcionar

--- codi/provacercabicing.py
+++ codi/provacercabicing.py
@@ -1,11 +1,20 @@
-import json
+
 import requests
 
+# URL de CityBikes per Bicing (Barcelona)
+url = "https://api.citybik.es/v2/networks/bicing"
 
+# Fem la crida
+response = requests.get(url)
+if response.status_code != 200:
+    print("Error en la connexió:", response.status_code)
+    exit()
 
-request = urllib2.Request('http://opendata-ajuntament.barcelona.cat/data/dataset/8214557a-.../resource/d6b5a09c-.../download/recurs.json')
-request.add_header('Authorization', '4f0d4dfaa06a4...')
-response = urllib2.urlopen(request)
+data = response.json()
 
-
-
+# Recorrem totes les estacions
+for station in data["network"]["stations"]:
+    print("Estació:", station["name"])
+    print("   -> Bicis lliures:", station["free_bikes"])
+    print("   -> Espais lliures:", station["empty_slots"])
+    print("-" * 40)
